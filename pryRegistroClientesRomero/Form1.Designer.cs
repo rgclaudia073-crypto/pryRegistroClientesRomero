8a79b965a797ff17846563b3cd6ac7bdcffdd863
santino <santinoch92@gmail.com>
2026-04-09 12:11:09 -0300

    Agregar archivos de proyecto.

---
+++ pryRegistroClientesRomero/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿using System;
+
+namespace pryRegistroClientesRomero
+{
+    partial class frmRegistroCliente
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpCargaDatos = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCódigo = new System.Windows.Forms.TextBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblLimiteCrédito = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblDeuda = new System.Windows.Forms.Label();
+            this.lblCódigo = new System.Windows.Forms.Label();
+            this.grpConsulta = new System.Windows.Forms.GroupBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnDeudores = new System.Windows.Forms.Button();
+            this.lblTotalDeudas = new System.Windows.Forms.Label();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpCargaDatos.SuspendLayout();
+            this.grpConsulta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpCargaDatos
+            // 
+            this.grpCargaDatos.Controls.Add(this.textBox3);
+            this.grpCargaDatos.Controls.Add(this.textBox2);
+            this.grpCargaDatos.Controls.Add(this.textBox1);
+            this.grpCargaDatos.Controls.Add(this.txtCódigo);
+            this.grpCargaDatos.Controls.Add(this.btnCargar);
+            this.grpCargaDatos.Controls.Add(this.lblLimiteCrédito);
+            this.grpCargaDatos.Controls.Add(this.lblUsuario);
+            this.grpCargaDatos.Controls.Add(this.lblDeuda);
+            this.grpCargaDatos.Controls.Add(this.lblCódigo);
+            this.grpCargaDatos.Location = new System.Drawing.Point(24, 35);
+            this.grpCargaDatos.Name = "grpCargaDatos";
+            this.grpCargaDatos.Size = new System.Drawing.Size(512, 194);
+            this.grpCargaDatos.TabIndex = 0;
+            this.grpCargaDatos.TabStop = false;
+            this.grpCargaDatos.Text = "Carga de Datos";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(392, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 24);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(392, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(101, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 10;
+            // 
+            // txtCódigo
+            // 
+            this.txtCódigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCódigo.Location = new System.Drawing.Point(101, 31);
+            this.txtCódigo.Name = "txtCódigo";
+            this.txtCódigo.Size = new System.Drawing.Size(98, 24);
+            this.txtCódigo.TabIndex = 9;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(345, 155);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(115, 33);
+            this.btnCargar.TabIndex = 8;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // lblLimiteCrédito
+            // 
+            this.lblLimiteCrédito.AutoSize = true;
+            this.lblLimiteCrédito.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLimiteCrédito.Location = new System.Drawing.Point(245, 106);
+            this.lblLimiteCrédito.Name = "lblLimiteCrédito";
+            this.lblLimiteCrédito.Size = new System.Drawing.Size(116, 18);
+            this.lblLimiteCrédito.TabIndex = 3;
+            this.lblLimiteCrédito.Text = "Limite de crédito";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(245, 30);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(60, 18);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblDeuda
+            // 
+            this.lblDeuda.AutoSize = true;
+            this.lblDeuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeuda.Location = new System.Drawing.Point(6, 106);
+            this.lblDeuda.Name = "lblDeuda";
+            this.lblDeuda.Size = new System.Drawing.Size(51, 18);
+            this.lblDeuda.TabIndex = 1;
+            this.lblDeuda.Text = "Deuda";
+            // 
+            // lblCódigo
+            // 
+            this.lblCódigo.AutoSize = true;
+            this.lblCódigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCódigo.Location = new System.Drawing.Point(5, 30);
+            this.lblCódigo.Name = "lblCódigo";
+            this.lblCódigo.Size = new System.Drawing.Size(56, 18);
+            this.lblCódigo.TabIndex = 0;
+            this.lblCódigo.Text = "Código";
+            // 
+            // grpConsulta
+            // 
+            this.grpConsulta.Controls.Add(this.btnListar);
+            this.grpConsulta.Controls.Add(this.btnDeudores);
+            this.grpConsulta.Controls.Add(this.lblTotalDeudas);
+            this.grpConsulta.Controls.Add(this.dgvClientes);
+            this.grpConsulta.Location = new System.Drawing.Point(33, 245);
+            this.grpConsulta.Name = "grpConsulta";
+            this.grpConsulta.Size = new System.Drawing.Size(502, 333);
+            this.grpConsulta.TabIndex = 1;
+            this.grpConsulta.TabStop = false;
+            this.grpConsulta.Text = "Consulta de Datos";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(360, 292);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(136, 35);
+            this.btnListar.TabIndex = 3;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeudores
+            // 
+            this.btnDeudores.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeudores.Location = new System.Drawing.Point(212, 292);
+            this.btnDeudores.Name = "btnDeudores";
+            this.btnDeudores.Size = new System.Drawing.Size(140, 35);
+            this.btnDeudores.TabIndex = 2;
+            this.btnDeudores.Text = "Listar Deudores";
+            this.btnDeudores.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalDeudas
+            // 
+            this.lblTotalDeudas.AutoSize = true;
+            this.lblTotalDeudas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalDeudas.Location = new System.Drawing.Point(235, 254);
+            this.lblTotalDeudas.Name = "lblTotalDeudas";
+            this.lblTotalDeudas.Size = new System.Drawing.Size(119, 20);
+            this.lblTotalDeudas.TabIndex = 1;
+            this.lblTotalDeudas.Text = "Total de deuda:";
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Usuario,
+            this.Column2,
+            this.Column3});
+            this.dgvClientes.Location = new System.Drawing.Point(6, 19);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.Size = new System.Drawing.Size(490, 223);
+            this.dgvClientes.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            // 
+            // Usuario
+            // 
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.Name = "Usuario";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Límite de crédito";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Deuda";
+            this.Column3.Name = "Column3";
+            // 
+            // frmRegistroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 590);
+            this.Controls.Add(this.grpConsulta);
+            this.Controls.Add(this.grpCargaDatos);
+            this.Name = "frmRegistroCliente";
+            this.Text = "Registro de Cliente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpCargaDatos.ResumeLayout(false);
+            this.grpCargaDatos.PerformLayout();
+            this.grpConsulta.ResumeLayout(false);
+            this.grpConsulta.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpCargaDatos;
+        private System.Windows.Forms.Label lblDeuda;
+        private System.Windows.Forms.Label lblCódigo;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblLimiteCrédito;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.GroupBox grpConsulta;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label lblTotalDeudas;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCódigo;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnDeudores;
+    }
+}
+
---
+++ pryRegistroClientesRomero/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryRegistroClientesRomero
+{
+    public partial class frmRegistroCliente : Form
+    {
+        public frmRegistroCliente()
+        {
+            InitializeComponent();
+        }
+        //Declaracion de un registro
+        private struct RegistroCliente
+        {
+            public Int32 Codigo;
+            public String Usuario;
+            public Decimal Deuda;
+            public Decimal Limite;
+        }
+        //Declaracion del vector
+        private RegistroCliente[] clientes = new RegistroCliente[10];
+
+        //Declaracion del indice
+        private Int32 IND = 0;
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (IND < clientes.Length)
+            {
+                Int32 i = 0;
+                while (ClienteS[IND].Codigo !=  Convert.ToInt32()
+        }
+    }
+}
